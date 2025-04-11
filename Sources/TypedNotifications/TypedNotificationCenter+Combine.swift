c6424e7443dc70687ed041c47c0884634f4fa9c7
mtj0928 <mtj0928@users.noreply.github.com>
2025-04-12 00:19:01 +0900

    Refine Interface

--- Sources/TypedNotifications/TypedNotificationCenter+Combine.swift
+++ Sources/TypedNotifications/TypedNotificationCenter+Combine.swift
@@ -3,13 +3,23 @@ import Combine
 import Foundation
 
 extension TypedNotificationCenter {
-
     public func publisher<Storage, Object: AnyObject>(
         for definition: TypedNotificationDefinition<Storage, Object>,
         object: Object? = nil
     ) -> some Publisher<TypedNotification<Storage, Object>, Never> {
         notificationCenter.publisher(for: definition.name, object: object)
             .compactMap { TypedNotification($0, basedOn: definition) }
+    }
+}
+
+extension NotificationCenter {
+    public func publisher<Storage, Object: AnyObject>(
+        for definition: TypedNotificationDefinition<Storage, Object>,
+        object: Object? = nil
+    ) -> AnyPublisher<TypedNotification<Storage, Object>, Never> {
+        publisher(for: definition.name, object: object)
+            .compactMap { TypedNotification($0, basedOn: definition) }
+            .eraseToAnyPublisher()
     }
 }
 #endif
